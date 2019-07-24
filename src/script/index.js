6eca2c6061c2ece034f49deac15f13f1b4a062bd
Bijay Yadav <bijay.yadav@cloudyfox.com>
2019-07-24 15:46:43 +0545

    Code Refactor.

--- src/script/index.js
+++ src/script/index.js
@@ -1,14 +1,15 @@
 $(function(){
-	$('#nav nav a').on('click', function(event) {
+	$('.menu nav a').on('click', function(event) {
 		$(this).parent().find('a').removeClass('active');
 		$(this).addClass('active');
 	});
+	
 	$(window).on('scroll', function() {
 		$('.target').each(function() {
 			if($(window).scrollTop() >= $(this).offset().top) {
 				var id = $(this).attr('id');
-				$('#nav nav a').removeClass('active');
-				$('#nav nav a[href="#'+ id +'"]').addClass('active');
+				$('.menu nav a').removeClass('active');
+				$('.menu nav a[href="#'+ id +'"]').addClass('active');
 			}
 		});
 	});
