16d9d07a5050f34677aab594d3b01e9307c237a6
Bijay Yadav <bijay.yadav@cloudyfox.com>
2019-07-26 14:22:27 +0545

    select the interest and hobbies section when reached last of the page.

--- src/script/index.js
+++ src/script/index.js
@@ -4,6 +4,7 @@ $(function(){
 		$(this).addClass('active');
 	});
 	
+
 	$(window).on('scroll', function() {
 		$('.target').each(function() {
 			if($(window).scrollTop() + 100 >= $(this).offset().top) {
@@ -12,5 +13,10 @@ $(function(){
 				$('.menu nav a[href="#'+ id +'"]').addClass('active');
 			}
 		});
+		
+		if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 50) {
+			$('.menu nav a:nth-last-child(2)').removeClass('active');
+			$(".menu nav a:last-child").addClass('active');
+		}
 	});
 });
